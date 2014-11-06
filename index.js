b4f46bbe403504949b7fca7b7dc352a3edaf5b41
Randall Degges <r@rdegges.com>
2014-11-05 21:14:06 -0800

    Adding a single API call.

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var logfmt = require('logfmt');
+
+var app = express();
+var jokes = fs.readFileSync('jokes.txt').toString().split('\n');
+
+app.use(logfmt.bodyParserStream());
+
+app.get('/', function(req, res) {
+  res.json({ joke: jokes[Math.floor(Math.random() * jokes.length)] });
+});
+
+app.listen(process.env.PORT || 3000);
